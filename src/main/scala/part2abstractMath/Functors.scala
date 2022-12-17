98d0df4e87e73c8cc901a6659fb2ec9075f52ccf
Charles <info@carloslaraai.com>
2022-12-17 07:29:45 -0800

    Implemented Functor for binary trees

--- src/main/scala/part2abstractMath/Functors.scala
+++ src/main/scala/part2abstractMath/Functors.scala
@@ -35,6 +35,20 @@ object Functors {
   // generic container of integers
   def do10x[F[_]](container: F[Int])(implicit functor: Functor[F]): F[Int] = functor.map(container)(_ * 10)
 
+  // TODO: define a functor for a binary tree (do not use Functor.instance)
+  sealed trait Tree[+T]
+  // binary tree node types
+  final case class Leaf[+T](value: T) extends Tree[T]
+  final case class Branch[+T](value: T, left: Tree[T], right: Tree[T]) extends Tree[T]
+
+  implicit object TreeFunctor extends Functor[Tree] {
+    // Recursive implementation for case Branch (not tail recursive -> will revisit during Monads)
+    override def map[A, B](fa: Tree[A])(f: A => B): Tree[B] = fa match {
+      case Leaf(value) => Leaf(f(value))
+      case Branch(value, left, right) => Branch(f(value), map(left)(f), map(right)(f))
+    }
+  }
+
 
   def main(args: Array[String]): Unit = {
 
@@ -44,5 +58,10 @@ object Functors {
     println(do10x(modifiedList))
     println(do10x(modifiedOption))
     println(do10x(modifiedTry))
+    // Wow, somehow the compiler needs the explicit [Tree] type parameter
+    val mappedLeaf = do10x[Tree](Leaf(2))
+    val mappedBranch = do10x[Tree](Branch(4, Leaf(1), Leaf(2)))
+    println(mappedLeaf)
+    println(mappedBranch)
   }
 }
