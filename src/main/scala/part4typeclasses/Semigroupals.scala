776978f7943e1e1338c374c58923911d93db21dc
Charles <info@carloslaraai.com>
2023-01-17 04:27:20 -0800

    Implemented Semigroupal[List] with product override

--- src/main/scala/part4typeclasses/Semigroupals.scala
+++ src/main/scala/part4typeclasses/Semigroupals.scala
@@ -25,13 +25,67 @@ object Semigroupals {
 
   // Why are Semigroupals useful??
 
-  import cats.instances.list._
+  import cats.instances.list._ // Monad[List]
 
   val aTupledList: List[(Int, String)] = Semigroupal[List].product(List(1, 2), List("a", "b")) // Cartesian product
 
+  // TODO: Implement with Monad
+  import cats.Monad
+  def productWithMonads[F[_], A, B](fa: F[A], fb: F[B])(implicit monad: Monad[F]): F[(A, B)] = {
+    monad.flatMap(fa)(a => monad.map(fb)(b => (a, b)))
+  }
+
+  import cats.syntax.functor._ // map
+  import cats.syntax.flatMap._ // flatMap
+  def cleanerProduct[F[_] : Monad, A, B](fa: F[A], fb: F[B]): F[(A, B)] = for {
+    a <- fa
+    b <- fb
+  } yield (a, b)
+
+  // MONADS EXTEND SEMIGROUPALS
+
+  // use case: Validated
+  import cats.data.Validated
+
+  type ErrorsOr[T] = Validated[List[String], T]
+
+  // implements the product of two Validated instances in terms of independently combining the error type and value type through Semigroup
+  val validatedSemigroupal: Semigroupal[ErrorsOr] = Semigroupal[ErrorsOr] // requires implicit Semigroup[List[_]]
+
+  val invalidsCombination: ErrorsOr[(Nothing, Nothing)] = validatedSemigroupal.product(
+    Validated.invalid(List("Something wrong", "Wrong again")),
+    Validated.invalid(List("This can't be right"))
+  )
+
+  // ***
+
+  type EitherErrorsOr[T] = Either[List[String], T]
+
+  import cats.instances.either._ // implicit Monad[Either]
+
+  val eitherSemigroupal: Semigroupal[EitherErrorsOr] = Semigroupal[EitherErrorsOr]
+
+  // implemented in terms of map/flatMap
+  val eitherCombination: EitherErrorsOr[(Nothing, Nothing)] = eitherSemigroupal.product(
+    Left(List("Something wrong", "Wrong again")),
+    Left(List("This can't be right"))
+  )
+  // Implementing error tracking with Monads is not good because we lose information
+
+  // TODO: define a Semigroupal[List] which does a zip
+  val zipSemigroupal: Semigroupal[List] = new Semigroupal[List] {
+    override def product[A, B](fa: List[A], fb: List[B]): List[(A, B)] = fa zip fb
+  }
+  val zippedList: List[(Int, String)] = zipSemigroupal.product(List(1, 2), List("a", "b"))
 
   def main(args: Array[String]): Unit = {
     //
     println(aTupledList)
+    println(productWithMonads(List(1, 2), List("a", "b")))
+    println(cleanerProduct(List(1, 2), List("a", "b")))
+    println(invalidsCombination)
+    // Second Either is missing because flatMap/map short-circuits (prevents propagation)
+    println(eitherCombination)
+    println(zippedList)
   }
 }
