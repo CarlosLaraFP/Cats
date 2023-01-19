14aef2a48f2fa19569f5f13f7a7a73fa8ac054ad
Charles <info@carloslaraai.com>
2023-01-19 06:49:15 -0800

    Implemented MonadError and ApplicativeError extension methods

--- src/main/scala/part4typeclasses/HandlingErrors.scala
+++ src/main/scala/part4typeclasses/HandlingErrors.scala
@@ -1,6 +1,6 @@
 package part4typeclasses
 
-import cats.Monad
+import cats.{Applicative, Monad}
 
 import java.util.concurrent.Executors
 import scala.concurrent.{ExecutionContext, Future}
@@ -11,8 +11,14 @@ object HandlingErrors {
 
   // JVM exception type (Throwable), or any other type that represents an error
 
-  trait MyMonadError[M[_], E] extends Monad[M] {
+  trait MyApplicativeError[M[_], E] extends Applicative[M] {
     def raiseError[A](e: E): M[A]
+    def handleErrorWith[A](ma: M[A])(f: E => M[A]): M[A]
+    def handleError[A](ma: M[A])(f: E => A): M[A] = handleErrorWith(ma)(e => pure(f(e)))
+  }
+
+  trait MyMonadError[M[_], E] extends Monad[M] {
+    def ensure[A](ma: M[A])(error: E)(predicate: A => Boolean): M[A]
   }
 
   import cats.MonadError
@@ -46,6 +52,28 @@ object HandlingErrors {
   implicit val ec: ExecutionContext = ExecutionContext.fromExecutorService(Executors.newFixedThreadPool(8))
   // Future which will complete with a Failure(exception)
   MonadError[Future, Throwable].raiseError(exception)
+
+  // Applicatives => ApplicativeError (useful for non-monadic types, such as Validated)
+  import cats.data.Validated
+  import cats.instances.list._ // implicit Semigroup[List] => ApplicativeError[ErrorsOr, List[String]]
+  type ErrorsOr[T] = Validated[List[String], T]
+  import cats.ApplicativeError
+  val applicativeError: ApplicativeError[ErrorsOr, List[String]] = ApplicativeError[ErrorsOr, List[String]]
+  // pure, raiseError, handleError, handleErrorWith
+
+  // Extension methods
+  import cats.syntax.applicative._ // pure
+  import cats.syntax.applicativeError._ // raiseError, handleError, handleErrorWith
+
+  val extendedSuccess: ErrorsOr[Int] = 42.pure[ErrorsOr] // requires the implicit ApplicativeError[ErrorsOr, List[String]]
+  val extendedError: ErrorsOr[Int] = List("Badness").raiseError[ErrorsOr, Int]
+  val recoveredError: ErrorsOr[Int] = extendedError.recover {
+    case _ => 43
+  }
+
+  import cats.syntax.monadError._ // ensure
+
+  val testedSuccess: ErrorOr[Int] = success.ensure("Something bad")(_ > 100)
 
 
   def main(args: Array[String]): Unit = {
