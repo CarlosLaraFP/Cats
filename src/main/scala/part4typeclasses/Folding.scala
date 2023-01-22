0019adf3b7ec7e5aa8a0c15a5bb7e7434450edb4
Charles <info@carloslaraai.com>
2023-01-22 05:15:37 -0800

    Implemented Foldable[A] composition for nested data structures

--- src/main/scala/part4typeclasses/Folding.scala
+++ src/main/scala/part4typeclasses/Folding.scala
@@ -2,7 +2,7 @@ package part4typeclasses
 
 import cats.Monoid
 import cats.syntax._
-import cats.instances.int._
+import cats.instances.int._ // Monoid[Int]
 
 object Folding {
 
@@ -21,13 +21,41 @@ object Folding {
     def combineAll[A](list: List[A])(implicit monoid: Monoid[A]): A = list.foldLeft(monoid.empty)(monoid.combine)
   }
 
+  import cats.Eval
+  import cats.Foldable
+
+  import cats.instances.list._ // implicit Foldable[List]
+  val six: Int = Foldable[List].foldLeft(List(1, 2, 3), 0)(_ + _) // 6
+
+  import cats.instances.option._ // implicit Foldable[Option]
+  val optionInt: Int = Foldable[Option].foldLeft(Option(2), 30)(_ + _) // 32
+
+  // foldRight with Eval guarantees tail recursion, regardless of the implementation of the container
+  // makes any container stack-safe by chaining Evals
+  val sum: Eval[Int] = Foldable[List].foldRight(List(1, 2, 3), Eval.now(0)) {
+    (num, eval) => eval.map(_ + num)
+  } // .value
+
+  val anotherSum: Int = Foldable[List].combineAll(List(1, 2, 3)) // implicit Monoid[Int]
+
+  import cats.instances.string._
+  val mappedConcat: String = Foldable[List].foldMap(List(1, 2, 3))(_.toString) // implicit Monoid[String]
+
+  val intNested: List[Vector[Int]] = List(Vector(1, 2, 3), Vector(4, 5, 6))
+  import cats.instances.vector._
+  val listVector: Int = (Foldable[List] compose Foldable[Vector]).combineAll(intNested)
+
 
   def main(args: Array[String]): Unit = {
     //
     val testList = List(5, 10, 15)
     println(ListExercises.map(testList)(_ * 2))
-    println(ListExercises.flatMap(testList)(a => List(a.getClass)))
+    println(ListExercises.flatMap(testList)(a => List(a, a, a)))
     println(ListExercises.filter(testList)(_ > 5))
     println(ListExercises.combineAll(testList))
+    println(six)
+    println(optionInt)
+    println(sum.value)
+    println(listVector)
   }
 }
