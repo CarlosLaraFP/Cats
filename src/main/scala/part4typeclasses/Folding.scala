85f65288c5d66c1324d4bd0510ebbd5bdf60c454
Charles <info@carloslaraai.com>
2023-01-22 04:53:06 -0800

    Tested 4 List methods

--- src/main/scala/part4typeclasses/Folding.scala
+++ src/main/scala/part4typeclasses/Folding.scala
@@ -9,13 +9,14 @@ object Folding {
   // TODO: Implement all in terms of foldLeft & foldRight
   object ListExercises {
     def map[A, B](list: List[A])(f: A => B): List[B] = list.foldRight(List.empty[B]) {
-      case (currentElement, currentList) => f(currentElement) :: currentList
+      (currentElement, currentList) => f(currentElement) :: currentList
     }
     def flatMap[A, B](list: List[A])(f: A => List[B]): List[B] = list.foldRight(List.empty[B]) {
-      case (currentElement, currentList) => f(currentElement) ::: currentList
+      (currentElement, currentList) => f(currentElement) ::: currentList
     }
     def filter[A](list: List[A])(predicate: A => Boolean): List[A] = list.foldRight(List.empty[A]) {
       case (currentElement, currentList) if predicate(currentElement) => currentElement :: currentList
+      case (_, currentList) => currentList
     }
     def combineAll[A](list: List[A])(implicit monoid: Monoid[A]): A = list.foldLeft(monoid.empty)(monoid.combine)
   }
@@ -23,7 +24,10 @@ object Folding {
 
   def main(args: Array[String]): Unit = {
     //
-
-    println(ListExercises.combineAll(List(5, 5, 5))) // 15
+    val testList = List(5, 10, 15)
+    println(ListExercises.map(testList)(_ * 2))
+    println(ListExercises.flatMap(testList)(a => List(a.getClass)))
+    println(ListExercises.filter(testList)(_ > 5))
+    println(ListExercises.combineAll(testList))
   }
 }
